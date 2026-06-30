43f587485bd1107a74766fd672474a6715bddd71
Akash B <akashspm5@gmail.com>
2026-06-30 12:02:54 +0530

    updated address

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -40,7 +40,7 @@ const Footer = () => {
                             </span>
                         </Link>
                         <p className="text-text-secondary text-sm">Full Stack Developer</p>
-                        <p className="text-text-secondary/50 text-xs mt-1 font-mono">Erode, India</p>
+                        <p className="text-text-secondary/50 text-xs mt-1 font-mono">Namakkal, TamilNadu, India</p>
                     </div>
 
                     {/* Nav links */}
